ecb9fd0a223b04386594622084db866500f70627
ArbenitBytyqi <arbenitbytyqi7@gmail.com>
2022-09-15 13:23:29 +0200

    test getting all posts

--- hoxton-node-medium-frontend/src/App.tsx
+++ hoxton-node-medium-frontend/src/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState<Posts[]>([]);
+  const [comments, setComments] = useState<Comments[]>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+      .then((resp) => resp.json())
+      .then((postsFromServer) => setPosts(postsFromServer));
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/posts")
+      .then((resp) => resp.json())
+      .then((usersFromServer) => setComments(usersFromServer));
+  }, []);
+
   return (
     <div className="App">
-      <div>
-        <h1>Front end</h1>
-      </div>
+      <main className="posts">
+        {posts.map((post) => (
+          <div>
+            <div>{post.id}</div>
+            <div>{post.title}</div>
+            <div>{post.content}</div>
+            <div>{post.image}</div>
+            <div>{post.likes}</div>
+          </div>
+        ))}
+      </main>
     </div>
   );
 }
